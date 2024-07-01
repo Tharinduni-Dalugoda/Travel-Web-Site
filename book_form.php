33a5c004401e73713446c0d4a4c3b912ce6f5843
Tharinduni <tharindunilakshani.9887@gmail.com>
2024-07-01 16:37:43 +0530

    Create header

---
+++ book_form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- fontawosome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- swiper link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- start header -->
+    <section class="header">
+        <a href="php.home" class="logo"> Travel. </a>
+
+        <nav class="navbar">
+            <a href="home.php"> Home </a>
+            <a href="about.php"> About </a>
+            <a href="package.php"> package </a>
+            <a href="book.php"> Book </a>
+        </nav>
+    </section>
+    
+
+
+
+
+
+
+
+    <!-- js link -->
+    <script src="js/script.js"></script>
+    <!-- swiperjs link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- fontawosome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- swiper link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+    <!-- boostrap link -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+
+    <style>
+        
+    </style>
+</head>
+
+<body>
+    <!-- start header -->
+    <section class="header">
+        <a href="home.php" class="logo"><b> Travel. </b> </a>
+
+        <nav class="navbar">
+            <a href="home.php"> Home </a>
+            <a href="about.php"> About </a>
+            <a href="package.php"> Package </a>
+            <a href="book.php"> Book </a>
+        </nav>
+
+        <div id="menu-btn" class="fa-solid fa-bars"></div>
+
+    
+
+    </section>
+    
+    
+
+
+
+
+
+
+
+    <!-- js link -->
+    <script src="script.js"></script>
+    <!-- swiperjs link -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+    <!-- boostrap link -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
+</body>
+</html>
